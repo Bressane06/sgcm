9f9561c9a50df2ba96ed342f1fc999f483a13e09
ArthurCoutinhoSI <arthur.r.coutinho@ufms.br>
2026-04-27 02:17:17 -0300

    feat: add Swagger support and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // esse trecho que roda o swagger
+  const config = new DocumentBuilder()
+    .setTitle('SGCM API')
+    .setDescription('API documentation for SGCM')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
