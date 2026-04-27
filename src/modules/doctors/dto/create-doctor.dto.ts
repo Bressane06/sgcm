07c92f41ba9d6d67d7875beb666d758b35dc1b7b
Bressane06 <gatongi44@gmail.com>
2026-04-27 15:34:50 -0300

    feat(users): refactor user module with entity separation and factory service for user creation

--- src/modules/doctors/dto/create-doctor.dto.ts
+++ src/modules/doctors/dto/create-doctor.dto.ts
@@ -4,5 +4,8 @@ import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
 export class CreateDoctorDto extends CreateUserDto {
   @IsString()
   @IsNotEmpty()
-  crm: string;
+
+  // declare = para evitar erro de TS sobre propriedade obrigatória 
+  // sem inicialização
+  declare crm: string; 
 }
