d33a2825d35c68a3fae6044a8d47c153f9c55481
Estela Medeiros <estela.meddeiros@gmail.com>
2026-05-03 14:53:12 -0400

    service, query filter e controller de pacientes

---
+++ src/modules/users/patients.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { PatientsService } from './services/patients.service';
+import { FindPatientsQueryDto } from './dto/find-patients-query.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+
+@ApiTags('Patients')
+@Controller('patients')
+export class PatientsController {
+  constructor(private readonly patientsService: PatientsService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Listar pacientes' })
+  findAll(@Query() query: FindPatientsQueryDto) {
+    return this.patientsService.findAll(query);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Buscar paciente por ID' })
+  findOne(@Param('id') id: number) {
+    return this.patientsService.findOne(Number(id));
+  }
+}
