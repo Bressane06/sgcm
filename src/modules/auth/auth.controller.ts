91f173b78e28d2f6356edd87b441138a154cc9d8
ArthurCoutinhoSI <arthur.r.coutinho@ufms.br>
2026-05-18 03:35:55 -0300

    feat(auth): implement login functionality and JWT token generation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
+import { User } from '../users/entities/user.entity';
+import { LocalAuthGuard } from './local-auth/local-auth.guard';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  login(@CurrentUser() user: User) {
+    return this.authService.login(user);
+  }
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,10 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { User } from '../users/entities/user.entity';
 import { compareSync } from 'bcrypt';
 import { UsersService } from '../users/services/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/user-token.model';
+import { UserPayload } from './models/user-payload.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(user: User): Promise<UserToken> {
+    const payload: UserPayload = {
+      sub: user.id, 
+      email: user.email,
+      name: user.name,
+      type: user.type,
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+      token_type: 'Bearer',
+    };
+  }
 
   async validateUser(email: string, pass: string): Promise<User | null> {
     const user = await this.usersService.findByEmail(email, true);
