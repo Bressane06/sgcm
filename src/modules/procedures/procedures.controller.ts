8c8410a989d97cb4bd45f2d2bfc91a31b528fb98
ArthurCoutinhoSI <arthurrcouti@email.com>
2026-06-02 12:32:13 -0400

    feat(procedures): enhance ProceduresController and ProceduresService to include user context for procedure operations

--- src/modules/procedures/procedures.controller.ts
+++ src/modules/procedures/procedures.controller.ts
@@ -6,39 +6,78 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { ProceduresService } from './procedures.service';
 import { CreateProcedureDto } from './dto/create-procedure.dto';
 import { UpdateProcedureDto } from './dto/update-procedure.dto';
+import { UserType } from '../users/enum/user-type.enum';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { ApiAuthResponses } from '../../common/swagger';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
+import type { UserPayload } from '../auth/models/user-payload.model';
 
-@Controller('procedures')
+@ApiAuthResponses({
+  instance: '/schedules',
+  unauthorizedDetail: 'Token JWT ausente, inválido ou expirado.',
+})
+@Controller('')
 export class ProceduresController {
   constructor(private readonly proceduresService: ProceduresService) {}
 
-  @Post()
-  create(@Body() createProcedureDto: CreateProcedureDto) {
-    return this.proceduresService.create(createProcedureDto);
+  @ApiTags('Appointments')
+  @Post('appointments/:appointmentId/procedures')
+  @Roles(UserType.ADMIN, UserType.DOCTOR)
+  create(
+    @Body() createProcedureDto: CreateProcedureDto,
+    @CurrentUser() user: UserPayload,
+  ) {
+    return this.proceduresService.create(createProcedureDto, user); // implementar quando appointment for criado
   }
 
-  @Get()
-  findAll() {
-    return this.proceduresService.findAll();
+  @ApiTags('Appointments')
+  @Get('appointments/:appointmentId/procedures')
+  @Roles(UserType.ADMIN, UserType.DOCTOR, UserType.PATIENT)
+  findAll(@CurrentUser() user: UserPayload) {
+    return this.proceduresService.findAll(user); // implementar quando appointment for criado
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.proceduresService.findOne(+id);
+  @ApiTags('Procedure')
+  @Get('procedures/:id')
+  @Roles(UserType.ADMIN, UserType.DOCTOR, UserType.PATIENT)
+  findOne(@Param('id') id: string, @CurrentUser() user: UserPayload) {
+    return this.proceduresService.findOne(+id, user);
   }
 
-  @Patch(':id')
+  @ApiTags('Procedure')
+  @Put('procedures/:id')
+  @Roles(UserType.ADMIN, UserType.DOCTOR)
   update(
     @Param('id') id: string,
     @Body() updateProcedureDto: UpdateProcedureDto,
+    @CurrentUser() user: UserPayload,
   ) {
-    return this.proceduresService.update(+id, updateProcedureDto);
+    return this.proceduresService.update(+id, updateProcedureDto, user);
   }
 
-  @Delete(':id')
+  @ApiTags('Procedure')
+  @Patch('procedures/:id/authorize')
+  @Roles(UserType.ADMIN)
+  authorizeProcedure(@Param('id') id: string) {
+    return this.proceduresService.authorizeProcedure(+id);
+  }
+
+  @ApiTags('Procedure')
+  @Patch('procedures/:id/deny')
+  @Roles(UserType.ADMIN)
+  denyProcedure(@Param('id') id: string) {
+    return this.proceduresService.denyProcedure(+id);
+  }
+
+  @ApiTags('Procedure')
+  @Delete('procedures/:id')
+  @Roles(UserType.ADMIN, UserType.DOCTOR)
   remove(@Param('id') id: string) {
     return this.proceduresService.remove(+id);
   }
--- src/modules/procedures/procedures.service.ts
+++ src/modules/procedures/procedures.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProcedureDto } from './dto/create-procedure.dto';
 import { UpdateProcedureDto } from './dto/update-procedure.dto';
+import { UserPayload } from '../auth/models/user-payload.model';
 
 @Injectable()
 export class ProceduresService {
-  create(createProcedureDto: CreateProcedureDto) {
+  create(createProcedureDto: CreateProcedureDto, currentUser: UserPayload) {
     return 'This action adds a new procedure';
   }
 
-  findAll() {
+  findAll(currentUser: UserPayload) {
     return `This action returns all procedures`;
   }
 
-  findOne(id: number) {
+  findOne(id: number, CurrentUser: UserPayload) {
     return `This action returns a #${id} procedure`;
   }
 
-  update(id: number, updateProcedureDto: UpdateProcedureDto) {
+  update(
+    id: number,
+    updateProcedureDto: UpdateProcedureDto,
+    currentUser: UserPayload,
+  ) {
     return `This action updates a #${id} procedure`;
+  }
+
+  authorizeProcedure(id: number) {
+    return `Autorizar procedimento especializado.`;
+  }
+
+  denyProcedure(id: number) {
+    return `Negar procedimento especializado.`;
   }
 
   remove(id: number) {
