285ae307b7f23941e81f52b827c7a03d514712e8
Bressane06 <gatongi44@gmail.com>
2026-06-08 10:06:22 -0300

    fix(users): adjust return email

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -166,7 +166,7 @@ export class UsersService {
   async findByEmail(
     email: string,
     includePassword: boolean = false,
-  ): Promise<User> {
+  ): Promise<User | null>{
     // Esse método será usado principalmente para autenticação, onde precisamos do hash da senha, por isso a opção includePassword.
 
     const user = await this.userRepository
@@ -176,7 +176,7 @@ export class UsersService {
       .getOne();
 
     if (!user) {
-      throw new NotFoundException('Usuário', email); // marcação de duvida: essa exceptiuon ta certa?
+      return null;
     }
 
     if (includePassword) {
